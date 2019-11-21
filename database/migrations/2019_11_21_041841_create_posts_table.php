2fd8aa90fcd6ca668b08ec7129de3b00c0517349
Juan Pablo Paillet <pailletjuanpablo@gmail.com>
2019-11-21 01:39:06 -0300

    Defined migrations

--- database/migrations/2019_11_21_041841_create_posts_table.php
+++ database/migrations/2019_11_21_041841_create_posts_table.php
@@ -15,6 +15,20 @@ class CreatePostsTable extends Migration
     {
         Schema::create('posts', function (Blueprint $table) {
             $table->bigIncrements('id');
+            $table->string('title');
+            // Con nullable() especificamos que el campo puede ser nulo
+            $table->text('content')->nullable();
+            $table->string('image')->nullable();
+            // Con default especificamos el valor por defecto de una columna
+            $table->boolean('published')->default(false);
+            $table->boolean('featured')->default(false);
+            // Relación con categorías
+            $table->unsignedBigInteger('category_id');
+            $table->foreign('category_id')->references('id')->on('categories');
+            // Relación con usuarios
+            $table->unsignedBigInteger('created_by');
+            $table->foreign('created_by')->references('id')->on('users');
+            // Timestamps
             $table->timestamps();
         });
     }
